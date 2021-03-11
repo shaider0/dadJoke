f49e49762762f0ee543e6ee36ad655280023bc86
SHaider0 <Samnoon.Haider@gmail.com>
2021-03-11 18:29:09 -0500

    successfully fetch data from icanhazdadjokes api using axios

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("connected")
+const getJoke = async () => {
+    const config = {
+        headers: {
+            Accept: "application/json"
+        }
+    }
+    let res = await axios.get("https://icanhazdadjoke.com/", config)
+    console.log(res)
+}
+const btn = document.querySelector("button")
+btn.addEventListener("click", () => {
+    getJoke()
+})
+const ul = document.querySelector("UL")
+const li = document.createElement("LI")
+
