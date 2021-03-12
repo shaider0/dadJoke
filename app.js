df8fdf48fe28827a1f6ea6017593cdf7695bc431
SHaider0 <Samnoon.Haider@gmail.com>
2021-03-12 13:01:28 -0500

    append jokes, add description of functionality

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const getJoke = async () => {
-    const config = {
-        headers: {
-            Accept: "application/json"
-        }
-    }
-    let res = await axios.get("https://icanhazdadjoke.com/", config)
-    console.log(res)
+const addJoke = async () => {
+    const res = await getJoke()
+    const jokeText = res.data.joke
+    const ul = document.querySelector("UL")
+    const li = document.createElement("LI")
+    li.append(jokeText)
+    ul.append(li)
 }
+
+const getJoke = async () => {
+    try {
+        const config = {
+            headers: {
+                Accept: "application/json"
+            }
+        }
+        let res = await axios.get("https://icanhazdadjoke.com/", config)
+        return res
+    } catch (error) {
+        console.log("Whoops, something went wrong")
+        console.log(error)
+    }
+
+}
+
 const btn = document.querySelector("button")
 btn.addEventListener("click", () => {
-    getJoke()
+    addJoke()
 })
-const ul = document.querySelector("UL")
-const li = document.createElement("LI")
-
